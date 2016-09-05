d9036528340423e60aae19e43ef7ea8349a05eb6
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-09-05 11:38:45 +0200

    List of movies - new function return_label

--- pages/movies.php
+++ pages/movies.php
@@ -10,6 +10,10 @@
 	function find_movie_index_by_name($name, $array) {
 		return array_search( $name, array_column( array_column($array, 'im:name'), 'label' ) );
 	}
+	
+	function return_label($label, $index, $array) {
+		return $array[$index][$label]['label'];
+	}
 ?>
 		<p><a href='../films.json'>Display the list of movies</a></p>
 		<p>Top 10 movies:</p>
@@ -17,7 +21,7 @@
 			<?php
 				for ( $index = 0; $index < 10; $index++ ) {
 					if ( $index > 0 ) { echo "\t\t\t"; }
-					echo '<li>'.$top[$index]['im:name']['label'].'</li>'."\n";
+					echo '<li>'.return_label('im:name', $index, $top).'</li>'."\n";
 				}
 			?>
 		</ol>
@@ -32,15 +36,15 @@
 			Director of "The LEGO Movie":<br />
 			<?php
 				$index = find_movie_index_by_name('The LEGO Movie', $top);
-				echo $top[$index]['im:artist']['label']."\n";
+				echo return_label('im:artist', $index, $top)."\n";
 			?>
 		</p>
 		<p>
 			Number of movies released before 2000:<br />
 			<?php
 				$count = 0;
-				foreach ( $top as $movie ) {
-					if ( explode('-', $movie['im:releaseDate']['label'])[0] < 2000 ) {
+				foreach ( $top as $index => $movie ) {
+					if ( explode('-', return_label('im:releaseDate', $index, $top))[0] < 2000 ) {
 						$count++;
 					}
 				}
